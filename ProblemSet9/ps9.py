432a8ffa7f51afce445d1ebe59d472e86a0e658d
stubear66 <stuart.kirschenbaum@gmail.com>
2013-04-26 10:42:13 -0400

    Final PS9 code

--- ProblemSet9/ps9.py
+++ ProblemSet9/ps9.py
@@ -7,7 +7,42 @@ from ps8b import *
 
 #
 # PROBLEM 1
-#        
+#    
+
+def runTrial(n, numViruses, maxPop, 
+             maxBirthProb, clearProb, resistances, mutProb):
+    additionalSteps = 150
+    virusList = []
+    for i in range(numViruses):
+        virusList.append(ResistantVirus(maxBirthProb, clearProb, resistances, mutProb))
+    p = TreatedPatient(virusList, maxPop)
+    totalSteps = n + additionalSteps
+    for step in range(totalSteps):
+       if step == n:
+           p.addPrescription('guttagonol')
+       pop = p.update()
+    
+    return pop
+
+def drawHist(results, numTrials):
+    yLabel = "Number of Trials"
+    xLabel = "Number of Viruses"
+    sp = [221,222,223,224]
+    i = 0
+    for n in sorted (results):
+        print n
+        pylab.subplot(sp[i])
+        popList = results[n]
+        pylab.title("Add drug at step " + str(n))
+        pylab.hist(popList, 20)
+        pylab.xlabel(xLabel)
+        pylab.ylabel(yLabel)
+        i += 1
+        
+
+    
+    pylab.show()      
+    
 def simulationDelayedTreatment(numTrials):
     """
     Runs simulations and make histograms for problem 1.
@@ -22,8 +57,36 @@ def simulationDelayedTreatment(numTrials):
     numTrials: number of simulation runs to execute (an integer)
     """
     
-    # TODO
+    #Initialization
+    #delayList = [300, 150, 75, 0]
+    delayList = [150]
+    #Patient init variables
+    numViruses = 100
+    maxPop = 1000
+    #Virus init variables
+    maxBirthProb = 0.1
+    clearProb = 0.05
+    #clearProb = 0.10
+    resistances = { 'guttagonol': True }
+    mutProb = 0.005
+    
+    results = {}
+    
+    for n in delayList:
+        cured = 0
+        popList = []
+        for i in range(numTrials):
+            pop = runTrial(n, numViruses, maxPop, maxBirthProb, clearProb, resistances, mutProb)
+            popList.append(pop)
+            if pop == 0:
+                cured +=1
+        results[n] = popList
+        #print popList
+        print "Delay : %(delay)d Percentage cured %(percent)2f" % {"delay" : n, "percent" : cured/float(numTrials) }
+        
 
+    drawHist(results, numTrials)   
+    
 
 
 
@@ -32,6 +95,27 @@ def simulationDelayedTreatment(numTrials):
 #
 # PROBLEM 2
 #
+
+def runTrialTwoDrugs (n, numViruses, maxPop, 
+                      maxBirthProb, clearProb, resistances, mutProb):
+    additionalSteps = 150
+    virusList = []
+    for i in range(numViruses):
+        virusList.append(ResistantVirus(maxBirthProb, clearProb, resistances, mutProb))
+    p = TreatedPatient(virusList, maxPop)
+    totalSteps = n + (additionalSteps * 2)
+    for step in range(totalSteps):
+       if step == additionalSteps:
+           #print "Adding guttagonol at step %(step)d" % {'step' : step}
+           p.addPrescription('guttagonol')
+       if step == (n + additionalSteps):
+            #print "Adding grimpex at step %(step)d" % {'step': step}
+            p.addPrescription('grimpex')  
+       pop = p.update()
+    
+    return pop
+    
+    
 def simulationTwoDrugsDelayedTreatment(numTrials):
     """
     Runs simulations and make histograms for problem 2.
@@ -45,4 +129,35 @@ def simulationTwoDrugsDelayedTreatment(numTrials):
 
     numTrials: number of simulation runs to execute (an integer)
     """
-    # TODO
+    #Initialization
+    delayList = [300, 150, 75, 0]
+    #delayList = [150]
+    #Patient init variables
+    numViruses = 100
+    maxPop = 1000
+    #Virus init variables
+    maxBirthProb = 0.1
+    clearProb = 0.05
+    #clearProb = 0.10
+    resistances = { 'guttagonol': False, 'grimpex' : False }
+    #mutProb = 0.005
+    mutProb = 0.010
+    
+    results = {}
+    
+    for n in delayList:
+        cured = 0
+        popList = []
+        print "Running trials for delay %(delay)d" % {'delay' : n}
+        for i in range(numTrials):
+            #print "Trial: " + str(i)
+            pop = runTrialTwoDrugs(n, numViruses, maxPop, maxBirthProb, clearProb, resistances, mutProb)
+            popList.append(pop)
+            if pop < 50:
+                cured +=1
+        results[n] = popList
+        #print popList
+        print "Delay : %(delay)d Percentage cured %(percent)2f" % {"delay" : n, "percent" : cured/float(numTrials) }
+        
+
+    drawHist(results, numTrials)   
